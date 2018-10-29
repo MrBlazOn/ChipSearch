1997240f6128465766943019a015f89264c22a3f
Egor Iskrenkov <mrblazon@gmail.com>
2018-10-30 01:38:21 +0300

    xpath parsing used

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,31 +1,34 @@
-require 'rubygems'
 require 'mechanize'
-require 'date'
-require 'json'
 
 class SearchController < ApplicationController
   def find_all_products
     agent = Mechanize.new
-    page = agent.get('http://belchip.by')
-    chipdip_form = page.form_with(:id => 'search_form')
-    chipdip_form.query = 'he'
-    page = agent.submit(chipdip_form, chipdip_form.buttons.first).css('.cat-item')
+    start_page = agent.get('http://belchip.by')
+    search_form = start_page.form_with(:id => 'search_form')
+    search_form.query = 'he'
+    search_page = agent.submit(search_form, search_form.buttons.first).parser
   end
 
   def get_info
-    products = find_all_products
-    info = [self.get_price(products), self.get_title(products), self.get_link(products)]
+    products = find_all_products.css('.cat-item')
+    info = [self.get_titles(products), self.get_prices(products), self.get_links(products)]
   end
 
-  def get_price products
-    products.css("div[class='denoPrice']")[0].text
+  def get_titles products
+    titles = []
+    products.css('h3').each { |product_title| titles.append(product_title.text)}
+    titles
   end
 
-  def get_title products
-    products.css('h3')[0].text
+  def get_prices products
+    prices = []
+    products.xpath("//div/span/div/div").each { |product_price| prices.append(product_price.text)}
+    prices
   end
 
-  def get_link products
-    product = products.xpath('//h3/a/@href')
+  def get_links products
+    links = []
+    products.xpath('//h3/a/@href').each { |product_link| links.append(product_link)}
+    links[1...links.length]
   end
 end
